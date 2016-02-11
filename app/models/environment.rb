7dd39cc8ea241e170f70f808dd9af02d259dbd47
Sergii Tkachenko <hi@sergii.org>
2016-02-11 23:07:46 +0200

    Automaticaly generate uuid for Environments.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -2,4 +2,8 @@ class Environment < ActiveRecord::Base
   belongs_to :project
   validates :project_id, presence: true
   validates :name, presence: true
+
+  before_create do
+    self.uuid = SecureRandom.uuid
+  end
 end
